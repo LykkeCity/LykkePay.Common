908b109397fd2b4d8b2c5f3896930ef00d71b72f
Vitalii Dasaev <vitalii.dasaev@lykke.com>
2018-07-31 11:12:49 +0300

    LPDEV-605 extract validation attributes

---
+++ src/LykkePay.Common/Validation/EmailAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Common;
+
+namespace LykkePay.Common.Validation
+{
+    /// <summary>
+    /// Check whether value is valid email
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class EmailAttribute : UseWithRequiredAttribute
+    {
+        private const string Message = "The {0} field is not a valid e-mail address.";
+
+        public override bool IsValid(object value)
+        {
+            if (IsRequiredResponsibility(value)) return true;
+
+            return value.ToString().IsValidEmail();
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(Message, name);
+        }
+    }
+}
---
+++ src/LykkePay.Common/Validation/ValidateModelAttribute.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Lykke.Common.ApiLibrary.Contract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace LykkePay.Common.Validation
+{
+    /// <summary>
+    /// Validates model and return ErrorMessage in case of any errors
+    /// </summary>
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var descriptor = context.ActionDescriptor as ControllerActionDescriptor;
+
+            if (descriptor != null)
+            {
+                var parameters = descriptor.MethodInfo.GetParameters();
+
+                foreach (var parameter in parameters)
+                {
+                    if (context.ActionArguments.Count == 0)
+                        continue;
+
+                    var argument = context.ActionArguments[parameter.Name];
+
+                    EvaluateValidationAttributes(parameter, argument, context.ModelState);
+                }
+            }
+
+            base.OnActionExecuting(context);
+
+            if (!context.ModelState.IsValid)
+            {
+                
+                context.Result = new BadRequestObjectResult(ErrorResponseFactory.Create(context.ModelState));
+            }
+        }
+
+        private void EvaluateValidationAttributes(ParameterInfo parameter, object argument, ModelStateDictionary modelState)
+        {
+            var validationAttributes = parameter.CustomAttributes;
+
+            foreach (var attributeData in validationAttributes)
+            {
+                var attributeInstance = CustomAttributeExtensions.GetCustomAttribute(parameter, attributeData.AttributeType);
+
+                var validationAttribute = attributeInstance as ValidationAttribute;
+
+                if (validationAttribute != null)
+                {
+                    var isValid = validationAttribute.IsValid(argument);
+                    if (!isValid)
+                    {
+                        modelState.AddModelError(parameter.Name, validationAttribute.FormatErrorMessage(parameter.Name));
+                    }
+                }
+            }
+        }
+    }
+}
